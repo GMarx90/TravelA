c8937124ea320d1a22ffca5a8272dc16b540d70d
kszymczyk94 <hooliganinq@gmail.com>
2020-01-27 23:04:27 +0100

    IJ fix

--- src/main/java/org/example/travel_agency/destination/App.java
+++ src/main/java/org/example/travel_agency/destination/App.java
@@ -5,20 +5,18 @@ import java.time.Period;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Country Hiszpania = new Country("Hiszpania");
-        City Barcelona =new City ("Barcelona",Hiszpania);
-        Hotel Stark = new Hotel(3.5,"B&b","Near to beach",Barcelona);
-        DepartCity Katowice =new DepartCity("Katowice");
-        Departure KTW =new Departure(Katowice, "Pyrzowice");
+        City Barcelona = new City("Barcelona", Hiszpania);
+        Hotel Stark = new Hotel(3.5, "B&b", "Near to beach", Barcelona);
+        DepartCity Katowice = new DepartCity("Katowice");
+        Departure KTW = new Departure(Katowice, "Pyrzowice");
         LocalDate dateOfDeparture = LocalDate.parse("2020-07-11");
         LocalDate dateOfReturn = LocalDate.parse("2020-07-25");
-        Trip trip1=new Trip(Stark,dateOfDeparture,dateOfReturn, KTW, 1890);
+        Trip trip1 = new Trip(Stark, dateOfDeparture, dateOfReturn, KTW, 1890);
         System.out.println(trip1.hotel);
+        System.out.println(trip1.getDateOfDeparture());
     }
 }
