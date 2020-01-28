88b4b13a251a2c8c44e1f49d4aac66346f5695d8
Grzegorz Marx <19grzegorz90@wp.pl>
2020-01-28 23:11:09 +0100

    Dodano baze danych do obrobienia

---
+++ src/main/Resources/db.migration/V8_add_CityAiroprt.sql
@@ -0,0 +1,6 @@
+INSERT INTO CityAiroprt (City_Airport_Name) value (Katowice);
+INSERT INTO CityAiroprt (City_Airport_Name) value (Warszawa);
+INSERT INTO CityAiroprt (City_Airport_Name) value (Kraków);
+INSERT INTO CityAiroprt (City_Airport_Name) value (Wrocław);
+INSERT INTO CityAiroprt (City_Airport_Name) value (Poznań);
+INSERT INTO CityAiroprt (City_Airport_Name) value (Gdańsk)
