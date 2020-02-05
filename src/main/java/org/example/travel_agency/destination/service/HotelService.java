a31f45cf04c761c14dd96ae08a4f6993b72bb800
GMarx90 <57858033+GMarx90@users.noreply.github.com>
2020-02-05 09:03:21 +0100

    Add files via upload

--- src/main/java/org/example/travel_agency/destination/service/HotelService.java
+++ src/main/java/org/example/travel_agency/destination/service/HotelService.java
@@ -1,1 +1,33 @@
-54917	54918	54919	54920	54921	54922	54923	54924	54925	54926	54927	54928	54929	54930	54931	54932	54933	54934	54935	54936	54937
+package org.example.travel_agency.destination.service;
+
+import org.example.travel_agency.destination.entities.Country;
+import org.example.travel_agency.destination.entities.City;
+import org.example.travel_agency.destination.entities.Hotel;
+import org.example.travel_agency.destination.entities.Trip;
+import org.example.travel_agency.destination.repository.CityRepository;
+import org.example.travel_agency.destination.repository.TripRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HotelService {
+
+    private HotelRepository hotelRepository;
+
+    @Autowired
+    public HotelService(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+
+    public List<Hotel> showAllHotels() {
+        return hotelRepository.findAll();
+    }
+
+    public boolean addHotel(Hotel hotel) {
+        hotelRepository.save(hotel);
+        return true;
+    }
+
+}
