7f019befab5628f2bb602aada8419dbfdf83b36d
GMarx90 <57858033+GMarx90@users.noreply.github.com>
2020-02-05 07:31:36 +0100

    Update Trip.java

--- src/main/java/org/example/travel_agency/destination/entities/Trip.java
+++ src/main/java/org/example/travel_agency/destination/entities/Trip.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.util.Date;
 
+@Getter @Setter
 @Entity
 @NoArgsConstructor
 public class Trip {
@@ -15,24 +16,17 @@ public class Trip {
     /*  HolidayDuration holiday jeszcze mozna dodac do konstruktora tylko jak to potem podpiac*/
 
     @OneToOne
-    @JoinColumn
-//    @JoinColumn(name= "hotel_id")
+    @JoinColumn(name= "hotelId") // zobacz czy działa, Dwa razy było Join column
     private Hotel hotel;
     private Date depart_Date;
     private Date return_Date;
 
     @OneToOne
-    @JoinColumn
-//    @JoinColumn(name="id_airport")
+    @JoinColumn(name="idAirport") //Zabacz czy działa, Dwa razy było Join column
     private Airport placeOfAirport;
 
 //    private HolidayDuration holiday;
     private int price;
-
-    public int getId_Trip() {
-
-        return id_Trip;
-    }
 
 
     public Trip(Hotel hotel, Date depart_Date, Date return_Date, Airport placeOfAirport, int price) {
@@ -44,53 +38,6 @@ public class Trip {
         this.price = price;
     }
 
-
-    public Hotel getHotel() {
-        return hotel;
-    }
-
-    public void setHotel(Hotel hotel) {
-        this.hotel = hotel;
-    }
-
-    public Date getDepart_Date() {
-        return depart_Date;
-    }
-
-    public void setDepart_Date(Date depart_Date) {
-        this.depart_Date = depart_Date;
-    }
-
-    public Date getReturn_Date() {
-        return return_Date;
-    }
-
-    public void setReturn_Date(Date return_Date) {
-        this.return_Date = return_Date;
-    }
-
-    public Airport getPlaceOfAirport() {
-        return placeOfAirport;
-    }
-
-    public void setPlaceOfAirport(Airport palceOfAirport) {
-        this.placeOfAirport = palceOfAirport;
-    }
-
-//    public HolidayDuration getHoliday() {
-//        return holiday;
-//    }
-
-//    public void setHoliday(HolidayDuration holiday) {
-//        this.holiday = holiday;
-//    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
+  
 }
 
