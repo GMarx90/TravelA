3589a08921387d550aef397d55d27907c2c0b2ff
kszymczyk94 <hooliganinq@gmail.com>
2020-02-06 22:16:26 +0100

    Update RController

--- src/main/java/org/example/travel_agency/destination/service/RController.java
+++ src/main/java/org/example/travel_agency/destination/service/RController.java
@@ -14,11 +14,12 @@ public class RController {
     private CountryService countryService;
     private CityService cityService;
     private HotelService hotelService;
-    private CityAirportService cityAirportService;   
+    private CityAirportService cityAirportService;
     private AirportService airportService;
     private OrdersService ordersService;
+    private AppUserService appUserService;
 
-    public RController(TripsService tripsService, CountryService countryService, CityService cityService, HotelService hotelService, CityAirportService cityAirportService, AirportService airportService, OrdersService ordersService) {
+    public RController(AppUserService appUserServive, TripsService tripsService, CountryService countryService, CityService cityService, HotelService hotelService, CityAirportService cityAirportService, AirportService airportService, OrdersService ordersService) {
         this.tripsService = tripsService;
         this.countryService = countryService;
         this.cityService = cityService;
@@ -26,80 +27,116 @@ public class RController {
         this.cityAirportService = cityAirportService;
         this.airportService = airportService;
         this.ordersService = ordersService;
+        this.appUserService = appUserServive;
     }
 
     @GetMapping("/countries")
     public List<Country> showAllC() {
-        return countryService.showAllCountries();}
-    
+        return countryService.showAllCountries();
+    }
+
     @GetMapping("/cities")
-     public List<Trip> showAllCities() {
-        return tripsService.showAllTrips();}
-    
+    public List<Trip> showAllCities() {
+        return tripsService.showAllTrips();
+    }
+
     @GetMapping("/hotels")
-      public List<Hotel> showAllHotels() {
-        return hotelService.showAllHotels();}
-    
+    public List<Hotel> showAllHotels() {
+        return hotelService.showAllHotels();
+    }
+
     @GetMapping("/departcities")
-      public List<CityAirport> showAllCityAirport() {
-        return cityAirportService.showAllCityAirport();}
-    
+    public List<CityAirport> showAllCityAirport() {
+        return cityAirportService.showAllCityAirport();
+    }
+
     @GetMapping("/airports")
-     public List<Airport> showAllAirports() {
-        return airportService.showAllAirports();}
-    
+    public List<Airport> showAllAirports() {
+        return airportService.showAllAirports();
+    }
+
     @GetMapping("/trips")
     public List<Trip> showAll() {
-        return tripsService.showAllTrips();}
+        return tripsService.showAllTrips();
+    }
 
     @GetMapping("/orders")
     public List<TripOrder> showAllOrders() {
-        return ordersService.showAllOrders();}
+        return ordersService.showAllOrders();
+    }
 
 
     @PostMapping("/country")
     public HttpStatus addCountry(@RequestBody Country country) {
         boolean b = countryService.addCountry(country);
-        if (b) {return HttpStatus.ACCEPTED;}
-        return HttpStatus.BAD_REQUEST;}
-    
+        if (b) {
+            return HttpStatus.ACCEPTED;
+        }
+        return HttpStatus.BAD_REQUEST;
+    }
+
     @PostMapping("/city")
-    public HttpStatus addCity (@RequestBody City city){
-        boolean b=cityService.addCity(city);
-            if (b){return HttpStatus.ACCEPTED;}
-            return HttpStatus.BAD_REQUEST;}
-    
+    public HttpStatus addCity(@RequestBody City city) {
+        boolean b = cityService.addCity(city);
+        if (b) {
+            return HttpStatus.ACCEPTED;
+        }
+        return HttpStatus.BAD_REQUEST;
+    }
+
     @PostMapping("/hotel")
-    public HttpStatus addHotel (@RequestBody Hotel hotel){
-        boolean b=hotelService.addHotel(hotel);
-        if (b){return HttpStatus.ACCEPTED;}
-        return HttpStatus.BAD_REQUEST;}
-    
+    public HttpStatus addHotel(@RequestBody Hotel hotel) {
+        boolean b = hotelService.addHotel(hotel);
+        if (b) {
+            return HttpStatus.ACCEPTED;
+        }
+        return HttpStatus.BAD_REQUEST;
+    }
+
     @PostMapping("/departcity")
-    public HttpStatus addCityAirport(CityAirport cityAirport){
-        boolean b=cityAirportService.addCityAirport(cityAirport);
-            if(b) {return HttpStatus.ACCEPTED;}
-            return HttpStatus.BAD_REQUEST;}
+    public HttpStatus addCityAirport(CityAirport cityAirport) {
+        boolean b = cityAirportService.addCityAirport(cityAirport);
+        if (b) {
+            return HttpStatus.ACCEPTED;
+        }
+        return HttpStatus.BAD_REQUEST;
+    }
 
     @PostMapping("/airport")
-    public HttpStatus addAirport (Airport airport){
-        boolean b=airportService.addAirport(airport);
-            if(b) {return HttpStatus.ACCEPTED;}
-            return HttpStatus.BAD_REQUEST;}
-        
+    public HttpStatus addAirport(Airport airport) {
+        boolean b = airportService.addAirport(airport);
+        if (b) {
+            return HttpStatus.ACCEPTED;
+        }
+        return HttpStatus.BAD_REQUEST;
+    }
+
     @PostMapping("/trip")
     public HttpStatus addTrip(@RequestBody Trip trip) {
         boolean b = tripsService.addTrip(trip);
-        if (b) {return HttpStatus.ACCEPTED;}
-        return HttpStatus.BAD_REQUEST;}
+        if (b) {
+            return HttpStatus.ACCEPTED;
+        }
+        return HttpStatus.BAD_REQUEST;
+    }
 
 
     @PostMapping("/order")
     public HttpStatus addOrder(@RequestBody TripOrder tripOrder) {
         boolean b = ordersService.addOrder(tripOrder);
-        if (b) {return HttpStatus.ACCEPTED;}
-        return HttpStatus.BAD_REQUEST;}
+        if (b) {
+            return HttpStatus.ACCEPTED;
+        }
+        return HttpStatus.BAD_REQUEST;
+    }
 
+    @PostMapping("/user")
+    public HttpStatus addUser(@RequestBody AppUser appUser) {
+        boolean b = appUserService.addAppUser(appUser);
+        if (b) {
+            return HttpStatus.ACCEPTED;
+        }
+        return HttpStatus.BAD_REQUEST;
+    }
 
-   
 }
